becbd1c91b35987ae982bdd30571c67791d51d95
Azka Sajid <f22ba031@ibitpu.edu.pk>
2026-06-15 16:24:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const images = document.querySelectorAll('.gallery img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.querySelector('.close');
+
+let currentIndex = 0;
+
+images.forEach((img,index)=>{
+
+    img.addEventListener('click',()=>{
+
+        lightbox.style.display='flex';
+        lightboxImg.src = img.src;
+        currentIndex = index;
+
+    });
+
+});
+
+closeBtn.addEventListener('click',()=>{
+
+    lightbox.style.display='none';
+
+});
+
+document.getElementById('next').addEventListener('click',()=>{
+
+    currentIndex = (currentIndex + 1) % images.length;
+    lightboxImg.src = images[currentIndex].src;
+
+});
+
+document.getElementById('prev').addEventListener('click',()=>{
+
+    currentIndex =
+    (currentIndex - 1 + images.length)
+    % images.length;
+
+    lightboxImg.src =
+    images[currentIndex].src;
+
+});
+
+function filterImages(category){
+
+    const allImages =
+    document.querySelectorAll('.image');
+
+    allImages.forEach(item=>{
+
+        if(category === 'all'){
+            item.style.display='block';
+        }
+        else if(item.classList.contains(category)){
+            item.style.display='block';
+        }
+        else{
+            item.style.display='none';
+        }
+
+    });
+}
